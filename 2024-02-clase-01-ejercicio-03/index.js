32bc6aa65c46c79f8d230dbe4d7d5ab09a280166
nacho <nacho.petroni@gmail.com>
2024-08-20 22:40:39 -0300

    ejercicio 3 resuelto

--- 2024-02-clase-01-ejercicio-03/index.js
+++ 2024-02-clase-01-ejercicio-03/index.js
@@ -1,20 +1,52 @@
-let notas;
-let alumno;
-let promedios;
-let maxNotas;
-let minNotas;
+// Contadores
+let contadorDesaprobados = 0, contadorAprobados = 0, contadorPromocionados = 0;
+// Acumuladores
+let acumuladorDesaprobados = 0, acumuladorAprobados = 0, acumuladorPromocionados = 0;
+// Variables para nota maxima y minima
+let maxNota = 1;
+let minNota = 10;
 
-
-// Ingreso el nombre del alumno
 do {
-    alumno = prompt('Ingrese el nombre del alumno');
-    document.write(`<h2>Las notas del alumno ${alumno} son:</h2>`);
-    
-// cuando el indice llegue a las 4 vueltas termina el ciclo
-    for(i = 0; i < 4; i++) {
-        let notas = parseInt(prompt('Ingrese las notas del alumno'));
-        document.write(`${notas}<br>`)
+    // 
+    let nota = parseInt(prompt('Ingrese una nota (1 al 10)'));
+
+    if (nota < 4) {
+        // desaprobado
+        contadorDesaprobados++;
+        acumuladorDesaprobados += nota;
+    } 
+    else {
+        // Aprobado
+        contadorAprobados++;
+        acumuladorAprobados += nota
+
+        //Pregunto si ademas, promociono
+        if(nota >= 7){
+            // SI,promociono
+            contadorPromocionados++;
+            acumuladorPromocionados += nota;
+        }
+    } 
+
+    // Preguntos si la nueva nota es mayor a la anterior
+    if(nota > maxNota){
+        // si esto es asi, encontre un nuevo maximo
+        maxNota =nota;
+    }
+    // Pregunta si la nueva nota es menor a la anterior
+    if (nota < minNota) {
+        // Si es asi, encontre un nuevo minimo
+        minNota = nota;
     }
 
-// Le indico al usuario si quiere ingresar las notas de otro alumno ya que llegue al limite de vueltas
-} while (confirm('Desea agregar las notas de otro alumno'));
+} while (confirm('Desea agregar otra nota?'));
+
+// Una vez que se cargaron las notas sacamos los promedios
+alert(`Hubo ${contadorDesaprobados} alumnos desaprobados y su nota promedio fue ${(acumuladorDesaprobados / contadorDesaprobados).toFixed(1)}`);
+
+alert(`Hubo ${contadorAprobados} alumnos aprobados y su nota promedio fue de ${(acumuladorAprobados / contadorAprobados).toFixed(1)}`);
+
+alert(`Hubo ${contadorPromocionados} alumnos prompcionados y su nota fue de ${(acumuladorPromocionados / contadorPromocionados).toFixed(1)}`);
+
+alert(`la nota minima del curso fue ${minNota}`);
+alert (`la nota maxima del curso fue ${maxNota}`);
